fbfad83f72808c11f77c66f3dca420a40a8d2d27
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-01-11 10:10:31 +0530

    NewChange

---
+++ basics01/src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Hellow!</div>;
+};
+
+export default Header;
---
+++ basics01/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Header from "./components/header";
+
+const App = () => (
+  <>
+    <Header />
+  </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
