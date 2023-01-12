cec4c4bb76aee893a32623bd04488ad90634d6bb
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-01-12 16:00:44 +0530

    props_03

--- basics01/src/index.js
+++ basics01/src/index.js
@@ -5,16 +5,20 @@ import JSON from "./db.json";
 
 import Header from "./components/header";
 import NewsList from "./components/news_list";
+import Footer from "./components/footer";
 
 class App extends Component {
   state = {
     news: JSON,
+    footerText:'This is Footer'
   };
   render() {
+    const { news,footerText } = this.state;
     return (
       <>
         <Header />
-        <NewsList news={this.state.news} />
+        <NewsList news={news} />
+        <Footer footerText={ footerText} />
       </>
     );
   }
