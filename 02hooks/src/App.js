45a4aa7124aba49dc35a495137f982128ca1069f
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-01-14 01:12:38 +0530

    useState02

--- 02hooks/src/App.js
+++ 02hooks/src/App.js
@@ -2,25 +2,80 @@ import React,{useState}  from "react";
 
 const App = ({initalCount}) => {
   
-  let [count, setCount] = useState(initalCount  );
+  // let [count, setCount] = useState(initalCount  );
+
+  const [state, setState] = useState({
+    count: initalCount,
+    name: 'shehan'
+  })
+
+  let [post, setPost] = useState([
+    {
+      name: 'This is first post',
+      body: 'First Body'
+    },
+    {
+      name: 'This is second post',
+      body: 'Second Body'
+    }
+  ])
+
+  const addPost = () => {
+    let newPost = {
+      name: 'New Post',
+      body: 'new Body'
+    }
+    setPost([
+      ...post,
+      newPost
+    ])
+  }
 
   const addOne = () => {
-    setCount(count+1);
+    setState({
+      ...state,
+      count: state.count + 1
+    });
    }
 
   const restOne = () => {
-    setCount(count - 1);
+    setState({
+      ...state,
+      count: state.count - 1
+    })
   }
    const resetOne = () => {
-    setCount(initalCount);
+     setState({
+       ...state,
+       count: state.count = initalCount
+     })
   }
    
   return (
     <>
-      <h3>count : { count}</h3>
+      <h2>{state.name }</h2>
+      <h3>count : {state.count}</h3>
       <button onClick={addOne}>Add one +1</button>
       <button onClick={restOne}>Rest one -1</button>
       <button onClick={resetOne}>Reset</button>
+      <div>
+        <button onClick={addPost}>ADD Post</button>
+      </div>
+      <hr/>
+      
+    
+        {post.map((item,i) => (
+          <div key={i}>
+            <div>
+              Name : {item.name}
+              <br></br>
+              Body : {item.body}
+              <hr/>
+            </div>
+          </div>
+        ))}
+ 
+
     </>
   );
 }
