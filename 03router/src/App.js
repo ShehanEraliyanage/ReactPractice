e6baeb0b5994c3aade54ed2be516acd81ddc7d43
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-01-20 20:19:33 +0530

    Update App.js

--- 03router/src/App.js
+++ 03router/src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import {BrowserRouter,Route} from "react-router-dom" ;
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home'
-import Posts from './components/Posts'
-import Profile from './components/profile'
+import Home from './components/Home';
+import Post from './components/Posts';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
-    <>
-      Hello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+        <Route path="/posts" element={<Post/> } />
+        <Route path="/profile" element={<Profile/> }/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
